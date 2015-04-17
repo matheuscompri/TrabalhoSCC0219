6d54c41edbef6cc0e39ce2a045c5f1b61e65a5f6
Matheus Compri <matheus.compri@gmail.com>
2015-04-16 22:43:29 -0300

    Validacao de email na pagina de registro

--- js/register.js
+++ js/register.js
@@ -6,145 +6,118 @@ window.onload = function () {
         var name = $("#name").val();
         var cpf = $("#cpf").val();
         var birth = $("#birth").val();
+        var password = $("#password").val();
+        var cep = $("#cep").val();
+        var email = $("#email").val();
 
         if (!is_name(name)) {
-            console.log(name + " " + is_name(name));
+            console.log("name: " + name + " " + is_name(name));
             valid = false;
         }
 
         if (!is_cpf(cpf)) {
-            console.log(cpf + " " + is_cpf(name));
-            valid = false;
-        }
-        
-        if (!is_date(birth)) {
-            console.log(birth + " " + is_date(birth));
+            console.log("cpf: " + cpf + " " + is_cpf(name));
             valid = false;
         }
 
+        if (!is_date(birth)) {
+            console.log("birthdate: " + birth + " " + is_date(birth));
+            valid = false;
+        }
+
+        if (!is_cep(cep)) {
+            console.log("cep: " + cep + " " + is_cep(cep));
+            valid = false;
+        }
+
+        if (is_password(password) === 'invalid') {
+            console.log("password: " + password + " " + is_password(password));
+            valid = false;
+        }
+        
+        if (!is_email(email)) {
+            console.log("email: " + email + " " + is_email(email));
+            valid = false;
+        }
+        
         if (!valid) {
             event.preventDefault();
         }
     });
 }
 
-function is_cep(cep)
-{
-    var cep_substring = cep.substr(0,3);
+function is_email(email) {
+    //RegExp to validate e-mail
+    var mailReg = /^[a-z](([a-z0-9.])*[^.])?@[a-z0-9]+([.][a-z]+)+$/;
+    return mailReg.test(email);
+}
+
+function is_cep(cep) {
+    if (cep === undefined || cep === '')
+        return false;
+
+    var cep_substring = cep.substr(0, 3);
     var state;
-    
+
     var select = $("#state").val();
-    
-    if((10 <= cep_substring) && (cep_substring <= 199))
-    {
+
+    if ((10 <= cep_substring) && (cep_substring <= 199)) {
         state = "SP";
-    }
-    else if((200 <= cep_substring) && (cep_substring <= 289))
-    {
+    } else if ((200 <= cep_substring) && (cep_substring <= 289)) {
         state = "RJ";
-    }
-    else if((290 <= cep_substring) && (cep_substring <= 299))
-    {
+    } else if ((290 <= cep_substring) && (cep_substring <= 299)) {
         state = "ES";
-    }
-    else if((300 <= cep_substring) && (cep_substring <= 399))
-    {
+    } else if ((300 <= cep_substring) && (cep_substring <= 399)) {
         state = "MG";
-    }
-    else if((400 <= cep_substring) && (cep_substring <= 489))
-    {
+    } else if ((400 <= cep_substring) && (cep_substring <= 489)) {
         state = "BA";
-    }
-    else if((490 <= cep_substring) && (cep_substring <= 499))
-    {
+    } else if ((490 <= cep_substring) && (cep_substring <= 499)) {
         state = "SE";
-    }
-    else if((500 <= cep_substring) && (cep_substring <= 569))
-    {
+    } else if ((500 <= cep_substring) && (cep_substring <= 569)) {
         state = "PE";
-    }
-    else if((570 <= cep_substring) && (cep_substring <= 579))
-    {
+    } else if ((570 <= cep_substring) && (cep_substring <= 579)) {
         state = "AL";
-    }
-    else if((580 <= cep_substring) && (cep_substring <= 589))
-    {
+    } else if ((580 <= cep_substring) && (cep_substring <= 589)) {
         state = "PB";
-    }
-    else if((590 <= cep_substring) && (cep_substring <= 599))
-    {
+    } else if ((590 <= cep_substring) && (cep_substring <= 599)) {
         state = "RN";
-    }
-    else if((600 <= cep_substring) && (cep_substring <= 639))
-    {
+    } else if ((600 <= cep_substring) && (cep_substring <= 639)) {
         state = "CE";
-    }
-    else if((640 <= cep_substring) && (cep_substring <= 649))
-    {
+    } else if ((640 <= cep_substring) && (cep_substring <= 649)) {
         state = "PI";
-    }
-    else if((650 <= cep_substring) && (cep_substring <= 659))
-    {
+    } else if ((650 <= cep_substring) && (cep_substring <= 659)) {
         state = "MA";
-    }
-    else if((660 <= cep_substring) && (cep_substring <= 688))
-    {
+    } else if ((660 <= cep_substring) && (cep_substring <= 688)) {
         state = "PA";
-    }
-    else if((690 <= cep_substring) && (cep_substring <= 692))
-    {
+    } else if ((690 <= cep_substring) && (cep_substring <= 692)) {
         state = "AM";
-    }
-    else if((694 <= cep_substring) && (cep_substring <= 698))
-    {
+    } else if ((694 <= cep_substring) && (cep_substring <= 698)) {
         state = "AM";
-    }
-    else if(689 === cep_substring)
-    {
+    } else if (689 === cep_substring) {
         state = "AP";
-    }
-    else if(693 === cep_substring)
-    {
+    } else if (693 === cep_substring) {
         state = "AR";
-    }
-    else if(699 === cep_substring)
-    {
+    } else if (699 === cep_substring) {
         state = "AC";
-    }
-    else if((700 <= cep_substring) && (cep_substring <= 736))
-    {
+    } else if ((700 <= cep_substring) && (cep_substring <= 736)) {
         state = "DF";
-    }
-    else if((728 <= cep_substring) && (cep_substring <= 767))
-    {
+    } else if ((728 <= cep_substring) && (cep_substring <= 767)) {
         state = "GO";
-    }
-    else if((770 <= cep_substring) && (cep_substring <= 779))
-    {
+    } else if ((770 <= cep_substring) && (cep_substring <= 779)) {
         state = "TO";
-    }
-    else if((780 <= cep_substring) && (cep_substring <= 788))
-    {
+    } else if ((780 <= cep_substring) && (cep_substring <= 788)) {
         state = "MT";
-    }
-    else if(789 == cep_substring)
-    {
+    } else if (789 == cep_substring) {
         state = "RO";
-    }
-    else if((800 <= cep_substring) && (cep_substring <= 879))
-    {
+    } else if ((800 <= cep_substring) && (cep_substring <= 879)) {
         state = "PR";
-    }
-    else if((880 <= cep_substring) && (cep_substring <= 889))
-    {
+    } else if ((880 <= cep_substring) && (cep_substring <= 889)) {
         state = "SC";
-    }
-    else if((900 <= cep_substring) && (cep_substring <= 999))
-    {
+    } else if ((900 <= cep_substring) && (cep_substring <= 999)) {
         state = "RS";
     }
-    
-    if(state === select)
+
+    if (state === select)
         return true;
     else
         return false;
@@ -164,11 +137,11 @@ function is_date(date) {
 
     // Regular expression for date
     var dateReg = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
-    
+
     // Testing if the regex is valid for the string
     if (dateReg.test(date)) {
         valid = true;
-        birthdate = new Date(date.substr(6,4), date.substr(3,2) - 1, date.substr(0,2));
+        birthdate = new Date(date.substr(6, 4), date.substr(3, 2) - 1, date.substr(0, 2));
     }
 
     // year after 1900 and before current year
@@ -226,66 +199,51 @@ function is_cpf(cpf) {
     }
 }
 
-function is_password(password)
-{
+function is_password(password) {
     var specialChars = /[\!\@\#\$\%\*?\,\;\.]/;
     var specCharList = [];
     var specCharCount = 0;
-    
+
     var capitalLetters = /[A-Z]/;
     var capitalCount = 0;
-    
+
     var numbers = /[0-9]/;
     var numberCount = 0;
-    
-    if(password.length < 6 || password.length > 12)
+
+    if (password.length < 6 || password.length > 12)
         return ("invalid");
-    if(password.length === 6)
+    if (password.length === 6)
         return ("weak");
-        
-    for(var i = 0; i <= password.length; i++)
-    {
-        if(specialChars.test(password[i]))
-        {
+
+    for (var i = 0; i <= password.length; i++) {
+        if (specialChars.test(password[i])) {
             specCharCount++;
             specCharList.push(password[i]);
-        }
-        else if(capitalLetters.test(password[i]))
-        {
+        } else if (capitalLetters.test(password[i])) {
             capitalCount++;
-        }
-        else if(numbers.test(password[i]))
-        {
+        } else if (numbers.test(password[i])) {
             numberCount++;
         }
     }
-    
+
     // Verifying if the special chars are unique
-    for(var i = 0; i < specCharList.length; i++)
-    {
-        for(var j = i + 1; j < specCharList.length -1; j++)
-        {
-            if(specCharList[i] === specCharList[j])
-            {
+    for (var i = 0; i < specCharList.length; i++) {
+        for (var j = i + 1; j < specCharList.length - 1; j++) {
+            if (specCharList[i] === specCharList[j]) {
                 // the special char i is not unique
                 specCharCount--;
             }
         }
     }
-    
+
     // Verifying if password is not weak
-    if(specCharCount && numberCount && capitalCount)
-    {
-         if(specCharCount >= 2)
-         {
-             return ("strong");
-         }
-        else{
+    if (specCharCount && numberCount && capitalCount) {
+        if (specCharCount >= 2) {
+            return ("strong");
+        } else {
             return ("medium");
         }
-    }
-    else
-    {
-        return("weak");
+    } else {
+        return ("weak");
     }
 }
