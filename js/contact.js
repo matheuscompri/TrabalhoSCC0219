a38d4d5ef9a275d12c083c446dbd239a974d795a
thatitis <thatitis@gmail.com>
2015-04-19 08:16:40 -0300

    Validacao da pagina de contato completa

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,9 @@
 window.onload = function () {
 
+    var validName = false;
+    var validMail = false;
+    var validPhone = false;
+
     $("#contactForm").focusout(function (event) {
 
         // Getting the name
@@ -9,31 +13,38 @@ window.onload = function () {
 
         if (is_name(name.val())) {
             valid(name);
+            validName = true;
+            
         } else {
             invalid(name);
+            validName = false;
         }
 
         if (is_mail(mail.val())) {
             valid(mail);
+            validMail = true;
+            
         } else {
             invalid(mail);
+            validMail = false;
         }
 
         if (is_phone(phone.val())) {
             console.log("valid");
             console.log(phone.val());
             var phoneReg = /^([(][0-9]{2}[)][0-9]{5}[-][0-9]{2}[-][0-9]{2})$/;
+            validPhone = true;
             if (phoneReg.test(phone.val())) {
                 valid(phone);
             }
         } else {
             console.log("invalid");
             invalid(phone);
-            console.log(phone.val());
+            validPhone = false;
         }
     });
-    
-    $("#contactForm").submit (function (event) {
+
+    $("#contactForm").submit(function (event) {
         //Getting access to the checkboxes status
         var news = $("#newspapers").is(":checked");
         var recc = $("#reccommendation").is(":checked");
@@ -41,20 +52,33 @@ window.onload = function () {
         var goog = $("#google_search").is(":checked");
         var magz = $("#magazines").is(":checked");
         var othr = $("#other").is(":checked");
-      
-        if(!(news||recc||ntwk||goog||magz||othr)){
-            if($("#warningCheck").length == 0){
+
+        var msg = $("#leaveMessage");
+
+        if (!(news || recc || ntwk || goog || magz || othr)) {
+            if ($("#warningCheck").length == 0) {
                 $("#checkOptions").append("<p id ='warningCheck' class='unchecked'>Please choose one of the options above</p>");
-           }
+                $("#warningCheck").css("color", "yellow");
+                $("#warningCheck").css("width", "60%");
+
+            }
+            event.preventDefault();
+        } else {
+            $("#warningCheck").remove();
+        }
+
+        if (is_empty(msg.val())) {
+            msg.attr("placeholder", "This field cannot be empty");
+            msg.css("border", "3px solid yellow");
             event.preventDefault();
         }
-        else{
-            $("#warningCheck").remove();
-            alert("OK");
+        
+        if(!(validMail && validName && validPhone)){
+            event.preventDefault();
         }
     });
-    
-    
+
+
 };
 
 function valid(name) {
@@ -85,4 +109,11 @@ function is_mail(name) {
 function is_phone(name) {
     var phoneReg = /^(|[(][0-9]{2}[)][0-9]{5}[-][0-9]{2}[-][0-9]{2})$/;
     return phoneReg.test(name);
+}
+
+function is_empty(name) {
+    if (name.length == 0) {
+        return true;
+    }
+    return false;
 }
